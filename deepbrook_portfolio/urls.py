a2ade0ad0a0e88684e259c02f5052ea39b29c601
[tdieff] <[tdieff@gmail.com]>
2020-10-08 20:26:24 -0400

    add paths for various pages

--- deepbrook_portfolio/urls.py
+++ deepbrook_portfolio/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name="deepbrook_portfolio"
 urlpatterns = [
-        path("", views.index, name="index")
+        path("", views.index, name="index"),
+        path("login", views.login, name="login"),
+        path("register", views.register, name="register"),
+        path("portfolio", views.portfolio, name="portfolio"),
+        path("qoute", views.quote, name="quote"),
+        path("buy", views.buy, name="buy"),
+        path("sell", views.sell, name="sell"),
+        path("history", views.history, name="history")
     ]
