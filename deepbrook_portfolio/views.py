717db2bf12dd9784e49a655116781ec8115a890b
[tdieff] <[tdieff@gmail.com]>
2020-10-08 20:27:12 -0400

    add functions to render various pages

--- deepbrook_portfolio/views.py
+++ deepbrook_portfolio/views.py
@@ -1,14 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
 
 # Create your views here.
 def index(request):
+    return render(request, "deepbrook_portfolio/index.html")
+
+def login(request):
+    return render(request, "deepbrook_portfolio/login.html")
+
+def register(request):
+    return render(request, "deepbrook_portfolio/register.html")
+
+def portfolio(request):
     some_username = "UserDude"
     some_dollar_value = 1000
     cash_amount = 17.09
-    return render(request, "deepbrook_portfolio/index.html", {
+    return render(request, "deepbrook_portfolio/portfolio.html", {
         "username": some_username,
         "portfolio_value": some_dollar_value,
         "cash": cash_amount,
         "total_assets": some_dollar_value + cash_amount
+    })
+
+def quote(request):
+    some_username = "UserDude"
+    return render(request, "deepbrook_portfolio/quote.html", {
+        "username": some_username,
+    })
+
+
+def buy(request):
+    some_username = "UserDude"
+    return render(request, "deepbrook_portfolio/buy.html", {
+        "username": some_username,
+    })
+
+
+def sell(request):
+    some_username = "UserDude"
+    return render(request, "deepbrook_portfolio/sell.html", {
+        "username": some_username,
+    })
+
+def history(request):
+    some_username = "UserDude"
+    return render(request, "deepbrook_portfolio/history.html", {
+        "username": some_username,
     })
