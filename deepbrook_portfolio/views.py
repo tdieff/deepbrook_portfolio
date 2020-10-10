b151ba6ad0de5782e1d49b029a59edadca7ffc1c
[tdieff] <[tdieff@gmail.com]>
2020-10-09 20:38:35 -0400

    given admin-created user, login works

--- deepbrook_portfolio/views.py
+++ deepbrook_portfolio/views.py
@@ -1,37 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from deepbrook_portfolio.forms import LoginForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse("login"))
     return render(request, "deepbrook_portfolio/index.html", {
         "username":"username"
     })
 
-def login(request):
+def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        # user = authenticate(request, username=username, password=password)
-        # if user:
-            # login(request, user)
-            # return HttpResponseRedirect((reverse("index"))
-        # else:
-            # return render(request, "deepbrook_portfolio/login.html", {
-                # "message":"Invalid login credentials"
-            # })
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return render(request, "deepbrook_portfolio/index.html")
     return render(request, "deepbrook_portfolio/login.html")
 
 def register(request):
     return render(request, "deepbrook_portfolio/register.html")
 
 def portfolio(request):
-    some_username = "UserDude"
-    some_dollar_value = 1000
-    cash_amount = 17.09
     return render(request, "deepbrook_portfolio/portfolio.html", {
         "username": some_username,
         "portfolio_value": some_dollar_value,
