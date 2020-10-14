25e5601facefbbff5b45ca4b4b6ff42585a645e0
[tdieff] <[tdieff@gmail.com]>
2020-10-14 15:28:49 -0400

    login and register successful

--- deepbrook_portfolio/views.py
+++ deepbrook_portfolio/views.py
@@ -1,15 +1,16 @@
+from django.db import IntegrityError
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
 from deepbrook_portfolio.forms import LoginForm
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from .models import User
+
 def index(request):
-    return render(request, "deepbrook_portfolio/index.html", {
-        "username":"username"
-    })
+    return render(request, "deepbrook_portfolio/index.html")
+
+def about(request):
+    return render(request, "deepbrook_portfolio/about.html")
 
 def login_view(request):
     if request.method == "POST":
@@ -19,42 +20,60 @@ def login_view(request):
         if user:
             login(request, user)
             return render(request, "deepbrook_portfolio/index.html")
-    return render(request, "deepbrook_portfolio/login.html")
+        else:
+            return render(request, "deepbrook_portfolio/login.html", {
+                "message": "Invalid login credentials. Try again:"
+            })
+    else:
+        return render(request, "deepbrook_portfolio/login.html", {
+            "message": "Log In"
+        })
+
+def logout_view(request):
+    logout(request)
+    return render(request, "deepbrook_portfolio/loggedout.html")
 
 def register(request):
-    return render(request, "deepbrook_portfolio/register.html")
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
 
+        if password != confirmation:
+            return render(request, "deepbrook_portfolio/register.html", {
+                "message": "Passwords do not match"
+            })
+
+        try:
+            email = None
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "deepbrook_portfolio/register.html", {
+                "message": "Username unavailable"
+            })
+        login(request, user)
+        return render(request, "deepbrook_portfolio/index.html")
+    else:
+        return render(request, "deepbrook_portfolio/register.html")
+
+
+@login_required
 def portfolio(request):
-    return render(request, "deepbrook_portfolio/portfolio.html", {
-        "username": some_username,
-        "portfolio_value": some_dollar_value,
-        "cash": cash_amount,
-        "total_assets": some_dollar_value + cash_amount
-    })
+    return render(request, "deepbrook_portfolio/portfolio.html")
 
+@login_required
 def quote(request):
-    some_username = "UserDude"
-    return render(request, "deepbrook_portfolio/quote.html", {
-        "username": some_username,
-    })
+    return render(request, "deepbrook_portfolio/quote.html")
 
-
+@login_required
 def buy(request):
-    some_username = "UserDude"
-    return render(request, "deepbrook_portfolio/buy.html", {
-        "username": some_username,
-    })
+    return render(request, "deepbrook_portfolio/buy.html")
 
-
+@login_required
 def sell(request):
-    some_username = "UserDude"
-    return render(request, "deepbrook_portfolio/sell.html", {
-        "username": some_username,
-    })
+    return render(request, "deepbrook_portfolio/sell.html")
 
+@login_required
 def history(request):
-    some_username = "UserDude"
-    return render(request, "deepbrook_portfolio/history.html", {
-        "username": some_username,
-    })
-
+    return render(request, "deepbrook_portfolio/history.html")
